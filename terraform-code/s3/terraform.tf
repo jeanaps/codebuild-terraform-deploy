2c0b169e2d650aa6bd98f1b715e3348bbe65676b
jeanaps <jean.aps12@gmail.com>
2019-12-18 17:49:47 -0300

    Repair repo

--- terraform-code/s3/terraform.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-2"
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "terraformbackend"
-}
